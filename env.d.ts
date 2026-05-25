d7ecfd68d271267a865691dcb75f7b686f77936b
Ernie Jeash <irenesejah29@gmail.com>
2026-05-26 02:42:03 +0800

    feat: declare VITE_SUPABASE_SERVICE_ROLE_KEY in env types

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_SUPABASE_URL: string;
+  readonly VITE_SUPABASE_KEY: string;
+  readonly VITE_SUPABASE_SERVICE_ROLE_KEY: string;
+  readonly VITE_MAPBOX_TOKEN: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
